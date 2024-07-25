5dbd95615b9a78665b991dc27cc99af4298e8497
rachidamrani <rachid3alaoui@gmail.com>
2024-07-25 13:41:16 +0200

    added notes repository

--- src/Api/Repository/NoteRepository.ts
+++ src/Api/Repository/NoteRepository.ts
@@ -1,28 +1,57 @@
+import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/Core/Datasource/Prisma";
-import Note from "../Entity/Note";
-import CreateOrUpdateNote from "../Dto/SaveNoteDto";
-import { NotImplementedException } from "@nestjs/common/exceptions";
+import { Note, Prisma } from "@prisma/client";
 
+@Injectable()
 export default class NoteRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async findById(noteId: number): Promise<Note> {
-    throw new NotImplementedException();
+    return await this.prisma.note.findUnique({
+      where: { id: noteId },
+    });
+  }
+
+  async findByUserId(userId: number): Promise<Note[]> {
+    return await this.prisma.note.findMany({
+      where: { userId },
+    });
   }
 
   async findMany(): Promise<Note[]> {
-    throw new NotImplementedException();
+    return await this.prisma.note.findMany();
   }
 
-  async create(userId: number, dto: CreateOrUpdateNote): Promise<Note> {
-    throw new NotImplementedException();
+  async save(
+    userId: number,
+    data:
+      | Prisma.XOR<Prisma.NoteCreateInput, Prisma.NoteUncheckedCreateInput>
+      | Prisma.XOR<Prisma.NoteUpdateInput, Prisma.NoteUncheckedUpdateInput>
+  ): Promise<Note> {
+    if (!data.id) {
+      return await this.prisma.note.create({
+        data: {
+          title: data.title as string,
+          content: data.content as string,
+          notebookId: data.notebookId as number,
+          userId: userId,
+        },
+      });
+    }
+
+    return await this.prisma.note.update({
+      where: {
+        id: data.id as number,
+      },
+      data: {
+        title: data.title as string,
+        content: data.content as string,
+        notebookId: data.notebookId as number,
+      },
+    });
   }
 
   async remove(noteId: number, userId: number): Promise<Note> {
-    throw new NotImplementedException();
-  }
-
-  async update(noteId: number, userId: number): Promise<Note> {
-    throw new NotImplementedException();
+    return this.prisma.note.delete({ where: { id: noteId } });
   }
 }
--- src/Api/Repository/Repositories.ts
+++ src/Api/Repository/Repositories.ts
@@ -1,4 +1,9 @@
 import NotebookRepository from "./NotebookRepository";
+import NoteRepository from "./NoteRepository";
 import UserRepository from "./UserRepository";
 
-export const Repositories = [UserRepository, NotebookRepository];
+export const Repositories = [
+  UserRepository,
+  NotebookRepository,
+  NoteRepository,
+];
