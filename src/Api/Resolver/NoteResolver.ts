9ecafb6d634b4fb7c397b8f47b22c3192126eac4
rachidamrani <rachid3alaoui@gmail.com>
2024-07-25 13:42:26 +0200

    added NoteResolver

--- src/Api/Resolver/NoteResolver.ts
+++ src/Api/Resolver/NoteResolver.ts
@@ -1,12 +1,17 @@
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
-import Note from "../Entity/Note";
-import { NotImplementedException, UseGuards } from "@nestjs/common";
-import GraphqlAuthGuard from "src/Core/Security/Guard/GraphqlAuthGuard";
+import { Args, Int, Mutation, Query, Resolver } from "@nestjs/graphql";
 import UseCaseFactory from "../UseCase/UseCaseFactory";
-import CreateNoteUseCase from "../UseCase/Note/CreateNote/CreateNoteUseCase";
-import CreateOrUpdateNote from "../Dto/SaveNoteDto";
-import { ContextualGraphqlRequest } from "src";
 import { ContextualRequest } from "src/Core/Decorator/ContextualRequest";
+import { ContextualGraphqlRequest } from "src";
+import Note from "../Entity/Note";
+import { UseGuards } from "@nestjs/common";
+import GraphqlAuthGuard from "src/Core/Security/Guard/GraphqlAuthGuard";
+import DeleteNoteUseCase from "../UseCase/Note/DeleteNote/DeleteNoteUseCase";
+import SaveNoteDto from "../Dto/SaveNoteDto";
+import CreateNoteUseCase from "../UseCase/Note/CreateNote/CreateNoteUseCase";
+import UpdateNoteUseCase from "../UseCase/Note/UpdateNote/UpdateNoteUseCase";
+import GetAllNotesUseCase from "../UseCase/Note/GetAllNotes/GetAllNoteUseCase";
+import GetNoteUseCase from "../UseCase/Note/GetNote/GetNoteUseCase";
+import GetNotesByUserIdUseCase from "../UseCase/Note/GetNotesByUserId/GetNotesByUserIdUseCase";
 
 @Resolver(Note)
 @UseGuards(GraphqlAuthGuard)
@@ -16,8 +21,64 @@ export default class NoteResolver {
   @Mutation(() => Note)
   async createNote(
     @ContextualRequest() context: ContextualGraphqlRequest,
-    @Args("dto") dto: CreateOrUpdateNote
+    @Args("dto") dto: SaveNoteDto
   ): Promise<Note> {
-    throw new NotImplementedException();
+    return (await this.serviceFactory.create(CreateNoteUseCase)).handle(
+      context,
+      dto
+    );
+  }
+
+  @Mutation(() => Note)
+  async updateNote(
+    @ContextualRequest() context: ContextualGraphqlRequest,
+    @Args("noteId", { type: () => Int }) noteId: number,
+    @Args("dto") dto: SaveNoteDto
+  ): Promise<Note> {
+    return (await this.serviceFactory.create(UpdateNoteUseCase)).handle(
+      context,
+      noteId,
+      dto
+    );
+  }
+
+  @Mutation(() => Note)
+  async deleteNote(
+    @ContextualRequest() context: ContextualGraphqlRequest,
+    @Args("noteId", { type: () => Int }) noteId: number
+  ): Promise<Note> {
+    return (await this.serviceFactory.create(DeleteNoteUseCase)).handle(
+      context,
+      noteId
+    );
+  }
+
+  @Query(() => Note)
+  async findNoteById(
+    @ContextualRequest() context: ContextualGraphqlRequest,
+    @Args("noteId", { type: () => Int }) noteId: number
+  ): Promise<Note> {
+    return (await this.serviceFactory.create(GetNoteUseCase)).handle(
+      context,
+      noteId
+    );
+  }
+
+  @Query(() => [Note])
+  async findNotes(
+    @ContextualRequest() context: ContextualGraphqlRequest
+  ): Promise<Note[]> {
+    return (await this.serviceFactory.create(GetAllNotesUseCase)).handle(
+      context
+    );
+  }
+
+  @Query(() => [Note])
+  async findNotesByUserId(
+    @ContextualRequest() context: ContextualGraphqlRequest
+  ): Promise<Note[]> {
+    return (await this.serviceFactory.create(GetNotesByUserIdUseCase)).handle(
+      context
+    );
   }
 }
--- src/Api/Resolver/Resolvers.ts
+++ src/Api/Resolver/Resolvers.ts
@@ -1,4 +1,5 @@
 import NotebookResolver from "./NotebookResolver";
+import NoteResolver from "./NoteResolver";
 import UserResolver from "./UserResolver";
 
-export const Resolvers = [UserResolver, NotebookResolver];
+export const Resolvers = [UserResolver, NotebookResolver, NoteResolver];
