d95235c2c133fecc8caa2b54b823c037d9c04d65
Omar Berrou <omar_berrou@hotmail.fr>
2024-10-30 14:22:02 +0100

    bugs fixed

--- src/Api/Repository/UserRepository.ts
+++ src/Api/Repository/UserRepository.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {Injectable} from "@nestjs/common";
 import {PrismaService} from "../../Core/Datasource/Prisma";
 import {Prisma} from "@prisma/client";
@@ -10,15 +9,6 @@ export default class UserRepository {
     private readonly prisma: PrismaService,
     private readonly bcrypt: Bcrypt
   ) {}
-=======
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../../Core/Datasource/Prisma";
-import { Prisma } from "@prisma/client";
-
-@Injectable()
-export default class UserRepository {
-  constructor(private readonly prisma: PrismaService) {}
->>>>>>> 18a8ae6 (add modif)
 
   async findById(id: number) {
     return this.prisma.user.findUnique({
@@ -32,7 +22,6 @@ export default class UserRepository {
     });
   }
 
-<<<<<<< HEAD
   async create(dto: Prisma.XOR<Prisma.UserCreateInput, Prisma.UserUncheckedCreateInput>) {
     const hashedPassword = await this.bcrypt.hash(dto.password);
     return this.prisma.user.create({
@@ -46,22 +35,6 @@ export default class UserRepository {
   }
 
   async save(data: Prisma.XOR<Prisma.UserUpdateInput, Prisma.UserUncheckedUpdateInput>) {
-=======
-  async saveUser(
-    data:
-      | Prisma.XOR<Prisma.UserCreateInput, Prisma.UserUncheckedCreateInput>
-      | Prisma.XOR<Prisma.UserUpdateInput, Prisma.UserUncheckedUpdateInput>
-  ) {
-    if (!data.id) {
-      return this.prisma.user.create({
-        data: data as Prisma.XOR<
-          Prisma.UserCreateInput,
-          Prisma.UserUncheckedCreateInput
-        >,
-      });
-    }
-
->>>>>>> 18a8ae6 (add modif)
     return this.prisma.user.update({
       where: { id: data.id as number },
       data: data as Prisma.XOR<Prisma.UserUpdateInput, Prisma.UserUncheckedUpdateInput>
