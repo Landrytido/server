6420af8aef10c737d8b3aeb9c5edbd5148fcb6f8
Omar Berrou <omar_berrou@hotmail.fr>
2024-12-18 22:24:45 +0100

    Conflict resolution

--- prisma/migrations/20241211211223_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `User` MODIFY `firstName` VARCHAR(255) NULL;
--- src/Core/Security/SecurityModule.ts
+++ src/Core/Security/SecurityModule.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule, JwtService } from '@nestjs/jwt';
@@ -21,28 +20,6 @@ import Login from './UseCase/Login';
 import LoginWithGoogle from './UseCase/LoginWithGoogle';
 import LoginWithFacebook from './UseCase/LoginWithFacebook';
 import LoginWithGithub from './UseCase/LoginWithGithub';
-=======
-import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { JwtModule, JwtService } from "@nestjs/jwt";
-import { PassportModule } from "@nestjs/passport";
-import UserRepository from "../../Api/Repository/UserRepository";
-import DatasourceModule from "../Datasource/DatasourceModule";
-import EventModule from "../Event/EventModule";
-import LoggingModule from "../Logging/LoggingModule";
-import AuthenticationEventEmitter from "./Event/AuthenticationEventEmitter";
-import AuthenticationEventSubscriber from "./Event/AuthenticationEventSubscriber";
-import LocalAuthGuard from "./Guard/LocalAuthGuard";
-import AuthenticationResolver from "./Resolver/AuthenticationResolver";
-import Authenticator from "./Service/authentication/Authenticator";
-import Bcrypt from "./Service/encryption/Bcrypt";
-import RequestTokenDecoder from "./Service/RequestTokenDecoder";
-import JwtStrategy from "./Strategy/JwtStrategy";
-import LocalStrategy from "./Strategy/LocalStrategy";
-import AuthenticationUseCaseFactory from "./UseCase/AuthenticationUseCaseFactory";
-import Login from "./UseCase/Login";
-import LoginWithGoogle from "./UseCase/LoginWithGoogle";
->>>>>>> origin/dev
 
 const authenticator = {
   provide: "Authenticator",
@@ -88,12 +65,8 @@ const authenticator = {
     RequestTokenDecoder,
     UserRepository,
     LoginWithGoogle,
-<<<<<<< HEAD
     LoginWithFacebook,
     LoginWithGithub,
   ]
-=======
-  ],
->>>>>>> origin/dev
 })
 export default class SecurityModule {}
