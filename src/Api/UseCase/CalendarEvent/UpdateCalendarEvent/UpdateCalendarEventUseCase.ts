55255370c29caf28ec5d7c56924283e4a40057da
Samypnotise <samy.djemili.dev@infyneis.com>
2025-02-25 10:01:59 +0100

    style(UpdateCalendarEvent): lint

--- src/Api/UseCase/CalendarEvent/UpdateCalendarEvent/UpdateCalendarEventUseCase.ts
+++ src/Api/UseCase/CalendarEvent/UpdateCalendarEvent/UpdateCalendarEventUseCase.ts
@@ -1,29 +1,60 @@
 // File: src/UseCase/CalendarEvent/UpdateCalendarEventUseCase.ts
 
-import { Injectable } from '@nestjs/common';
-import { ContextualGraphqlRequest, UseCase } from '../../../../index';
-import CalendarEventRepository from '../../../Repository/CalendarEvent/CalendarEventRepository';
-import { CalendarEvent } from '@prisma/client';
-import { UpdateCalendarEventDto } from '../../../Dto/CalendarEventDto/UpdateCalendarEventDto';
-import GoogleCalendarService from '../../../Services/GoogleCalendarService';
+import { Injectable } from "@nestjs/common";
+import { ContextualGraphqlRequest, UseCase } from "../../../../index";
+import CalendarEventRepository from "../../../Repository/CalendarEvent/CalendarEventRepository";
+import { CalendarEvent, CalendarEventType } from "@prisma/client";
+import { UpdateCalendarEventDto } from "../../../Dto/CalendarEventDto/UpdateCalendarEventDto";
+import GoogleCalendarService from "../../../Services/GoogleCalendarService";
 
 @Injectable()
-export default class UpdateCalendarEventUseCase implements UseCase<Promise<CalendarEvent>, [id: number, dto: UpdateCalendarEventDto]> {
-    constructor(
-	    private readonly calendarEventRepository: CalendarEventRepository,
-	    private readonly googleCalendarService: GoogleCalendarService,
-    ) {}
+export default class UpdateCalendarEventUseCase
+  implements
+    UseCase<Promise<CalendarEvent>, [id: number, dto: UpdateCalendarEventDto]>
+{
+  constructor(
+    private readonly calendarEventRepository: CalendarEventRepository,
+    private readonly googleCalendarService: GoogleCalendarService,
+  ) {}
 
-    async handle(context: ContextualGraphqlRequest, id: number, dto: UpdateCalendarEventDto): Promise<CalendarEvent> {
-	  // Update the local event record
-	  const event = await this.calendarEventRepository.update(context.userId, id, dto);
+  async handle(
+    context: ContextualGraphqlRequest,
+    id: number,
+    dto: UpdateCalendarEventDto,
+  ): Promise<CalendarEvent> {
+    // Fetch the previous event record
+    const previousEvent = await this.calendarEventRepository.findById(id);
 
-	  // Immediately push the updated event/task to Google
-	  if (event.eventType === 'EVENT') {
-		await this.googleCalendarService.updateGoogleCalendarEvent(context.userId, event);
-	  } else if (event.eventType === 'TASK') {
-		await this.googleCalendarService.updateGoogleTask(context.userId, event);
-	  }
-	  return event;
+    // Update the local event record
+    const event = await this.calendarEventRepository.update(
+      context.userId,
+      id,
+      dto,
+    );
+
+    if (previousEvent.eventType !== dto.eventType) {
+      if (previousEvent.eventType === CalendarEventType.EVENT) {
+        await this.googleCalendarService.deleteGoogleCalendarEvent(
+          context.userId,
+          previousEvent,
+        );
+      } else {
+        await this.googleCalendarService.deleteGoogleTask(
+          context.userId,
+          previousEvent,
+        );
+      }
     }
+
+    // Immediately push the updated event/task to Google
+    if (event.eventType === CalendarEventType.EVENT) {
+      await this.googleCalendarService.updateGoogleCalendarEvent(
+        context.userId,
+        event,
+      );
+    } else {
+      await this.googleCalendarService.updateGoogleTask(context.userId, event);
+    }
+    return event;
+  }
 }
