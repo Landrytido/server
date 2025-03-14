de2cad4e4ca8ef5f367fbd8e8fff9da5c8eaa8a0
Landrytido <atla2804@student.eafc-uccle.be>
2025-03-14 09:26:43 +0100

    fix merge

--- src/Api/UseCase/UseCaseFactory.ts
+++ src/Api/UseCase/UseCaseFactory.ts
@@ -2,12 +2,7 @@ import { Injectable } from "@nestjs/common";
 import ServiceFactory from "../../Core/Factory/ServiceFactory";
 
 import { AvailableUserUseCases } from "./User/AvailableUserUseCases";
-import { AvailableLinkGroupUseCases } from "./LinkGroup/AvailableLinkGroupUseCases";
-import { AvailableLinkUseCases } from "./Link/AvailableLinkUseCases";
-import { AvailableLinkClickUseCases } from "./LinkClick/AvailableLinkClickUseCases";
-import { AvailableEventUseCases } from "./Event/AvailableEventUseCases";
 import { AvailableTagUseCases } from "./Tag/AvailableTagUseCases";
-import { AvailableTaskUseCases } from "./Task/AvailableTaskUseCases";
 import { AvailableNoteUseCases } from "./Note/AvailbaleNoteUseCases";
 import { AvailableNotebookUseCases } from "./Notebook/AvailableNotebookUseCases";
 import { AvailableInvitationUseCases } from "./Invitation/AvailableInvitationUseCases";
@@ -15,33 +10,29 @@ import { AvailableSearchHistoryUseCases } from "./SearchHistory/AvailableSearchH
 import { AvailableCommentUseCases } from "./Comment/AvailableCommentUseCases";
 import { AvailableNoteTaskUseCases } from "./NoteTasks/AvailableNoteTaskskUseCases";
 import { AvailableFileUseCases } from "./File/AvailableFileUseCases";
-import { AvailableMeetingUseCases } from "./Meeting/AvailableMeetingUseCase";
 import { AvailableNotificationPreferenceUseCases } from "./Notifications/NotificationPreference/AvailableNotificationPreferenceUseCases";
 import { AvailableDailyTaskUseCases } from "./DailyTask/AvailableDailyTaskUseCases";
 import { AvailableCalendarEventUseCases } from "./CalendarEvent/AvailableCalendarEventUseCases";
 import { AvailableChronometerUseCases } from "./Chronometer/AvailableChronometerUseCases";
+import { AvailableLinksUseCases } from "./Links/AvailableLinksUseCases";
+import { AvailableAutoInstructionSuggestionUseCases } from "./AutoInstructionSuggestion/AvailableAutoInstructionSuggestionUseCases";
 
 export type UseCases =
   | AvailableUserUseCases
-  | AvailableEventUseCases
   | AvailableTagUseCases
-  | AvailableTaskUseCases
   | AvailableNotebookUseCases
   | AvailableNoteUseCases
   | AvailableInvitationUseCases
-  | AvailableLinkGroupUseCases
-  | AvailableLinkUseCases
-  | AvailableLinkClickUseCases
   | AvailableSearchHistoryUseCases
   | AvailableCommentUseCases
-  | AvailableMeetingUseCases
   | AvailableNoteTaskUseCases
   | AvailableNotificationPreferenceUseCases
   | AvailableDailyTaskUseCases
   | AvailableCalendarEventUseCases
+  | AvailableAutoInstructionSuggestionUseCases
   | AvailableFileUseCases
+  | AvailableLinksUseCases
   | AvailableChronometerUseCases;
-
 
 @Injectable()
 export default class UseCaseFactory extends ServiceFactory<UseCases> {}
